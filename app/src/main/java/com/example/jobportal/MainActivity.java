9bdbd588beb53d01cd8269f632e977fa7fefcc2b
maimansour129 <maimansour129@outlook.com>
2022-12-05 23:32:09 +0200

    z

--- app/src/main/java/com/example/jobportal/MainActivity.java
+++ app/src/main/java/com/example/jobportal/MainActivity.java
@@ -15,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
         TextView txt=(TextView) findViewById(R.id.txt1);
 
         txt.setText("Byeeee");
+        System.out.println(".");
     }
 }
