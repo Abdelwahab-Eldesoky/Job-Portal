3d484e036ddd08fdcb1d9455facfc1ed01ba8bc7
Abdelwahab-Eldesoky <abdelwahabnabil2001@gmail.com>
2022-12-17 22:45:25 +0200

    Search on offers and some layout modifications

--- app/src/main/java/com/example/jobportal/AvailableJobOffers.java
+++ app/src/main/java/com/example/jobportal/AvailableJobOffers.java
@@ -5,8 +5,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class AvailableJobOffers extends AppCompatActivity {
 
@@ -15,6 +21,7 @@ public class AvailableJobOffers extends AppCompatActivity {
     AllOffersRecyclerViewAdapter adapter;
     final PortalDB database=new PortalDB(this);
     String username;
+    EditText searchTxt;
 
 
     @Override
@@ -24,6 +31,8 @@ public class AvailableJobOffers extends AppCompatActivity {
 
         username=getIntent().getStringExtra("userName");
 
+        searchTxt=(EditText)findViewById(R.id.txtSearch);
+
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -32,6 +41,36 @@ public class AvailableJobOffers extends AppCompatActivity {
 
         adapter=new AllOffersRecyclerViewAdapter(this,list,username);
         recyclerView.setAdapter(adapter);
+
+        searchTxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                List<jobVacancy> tmpList=new ArrayList<>();
+                if (TextUtils.isEmpty(searchTxt.getText().toString())){
+                    adapter=new AllOffersRecyclerViewAdapter(getApplicationContext(),list,username);
+                    recyclerView.setAdapter(adapter);
+                }
+                else{
+                    for(jobVacancy vacancy:list){
+                        if(vacancy.getTittle().toLowerCase(Locale.ROOT).contains(searchTxt.getText().toString().toLowerCase(Locale.ROOT))){
+                            tmpList.add(vacancy);
+                        }
+                    }
+                    adapter=new AllOffersRecyclerViewAdapter(getApplicationContext(),tmpList,username);
+                    recyclerView.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
 
     }
 }
